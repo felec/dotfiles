ef2014ca27f413c5409789fb1ee26ae91539cb56
fele <43732967+felec@users.noreply.github.com>
2023-05-08 21:12:12 -0700

    :art: Resize chatgpt ascii

--- nvim/lua/plugins/chatgpt.lua
+++ nvim/lua/plugins/chatgpt.lua
@@ -10,47 +10,41 @@ return {
     opts = {
       chat = {
         welcome_message = [[
-                                .^
-                              :?J
-                            .?PJ
-                       .:~!7P&YGGJJ?!~:.
-                  .^!J555YJ?7!!!!77?JY55J!:
-               .!YGG5?!~~~~~~~~~~~~~~~~!7YP57.
-            .~5##P?~~~~~~~~~~~~~~~~~~~~~~~~75B?.
-          ..7@#Y?!~~~~~~~~~~~~~~~~~~~~~~~~~~.^GB:
-         5PYYYJ???!~~~~~~~~~~~~~~~~~~~~~~~~~. ?&:     SIR, YES SIR!
-        ~#JJY55Y?J7~~~~~~~~~~~~~~~~~~~~~~~~~  PG
-        !P:~^P@J77~~~~~~~~~~~~~~~~~~~~~~~~~^  BY
-        ?55@B#@GY?!~~~~~~~~~~~~~~~~~~~~~~~~. .&5?J5PBY
-        YPP&PPPGBBPYJ7!~~~~~~~~~~~~~~~!!7?J?J5BBBGGB@J
-        YGY@GPPPPPPPGGP5Y?7!~~~~~~JY55PPPPPPPPPPPPP#&:
-        ?B?B#PPPPPPPPPPPPPPPP~~~~~YPPPPPPPPPPPPPPPB@7
-        ~B5.&#PPPPPPPPPPPPPBY~~~~~!PGPPPPPPPPPPPP#&7
-        .P5I &@BPPPPPPPPPPPJ~~~~~~~!P#GPPPPPPPPB&G^
-         7PP  P@#GGGGPPPYJ!~~~~~~~~~~?5PGGP55GBP!
-          PYP. G@#55YJ?!~~~~~~~~~~~~~~~!7JJ5@5:
-          ^B55. ?@J77~~~~~~~~~~~|!!!|~~~~~~~GG.
-           7#YP: ?#Y77!~~~~~~~~~    ~~~~~!GP.
-            ?#JP!  GGY?7!!~~~~~~___~~~~!YG7
-             7BY55  !PGPJ?77!77!!!!!?JPP?.
-              ^GPJG   ^7Y55PGY!GP555Y?^
-               .?GJ5PPP55?J5P5JP5YJ??7J!
-                 :5PPGPPBPPY7????7?JPPG#
-                   ~GBGGPYGPPPYPP5Y7YPB?
-                      Y#P5BGPPPPPPP7YJG:
-                        G5BGPPPPPPP77P5!
-                         YGG5JJJJYY?  ?!
-                          YYYJ??777   7
-                          BPPP555PG   V
-                           ?JJJ?7~:
-                         JG       ?J
-                        7GG~      5#J
-                       :B7YJ     .B?B~
-                       YP~7G     ~#7YP
-                      .#J~~B^    5G7!G:
-                      ^&J~^PY   ^@Y7~B^
-                      :GYJ?YP   ?BYJJ5.
-                                 ]],
+                                ~J                        
+                              5?:                         
+                          :JY5~                           
+                     .:^~?#5P577!^:                       
+                 :~?YYYJ??7!!77??JYY?~.                   
+              :?PG5?!~~~~~~~~~~~~~~!7Y5J:                 
+            :P#GJ!~~~~~~~~~~~~~~~~~~~~~7GJ.               
+          ??JBY77~~~~~~~~~~~~~~~~~~~~~~ :@~               
+         JBJJYYJJ7~~~~~~~~~~~~~~~~~~~~^ ^#:   SIR,YES SIR!
+         J?!!B&?7~~~~~~~~~~~~~~~~~~~~~: 7G                
+         5Y&BB&PY?~~~~~~~~~~~~~~~~~~~~:.5#YPPBG           
+         P5&GPPPGGP5J?!~~~~~~~~!7?JY555PGGGPP&5           
+         P5G#PPPPPPPPPPP5Y!~~~7GPPPPPPPPPPPPB&            
+         JG7&BPPPPPPPPPPGG!~~~~5PPPPPPPPPPPB#             
+         ~PJ &&GPPPPPPPP57~~~~~!5BGPPPPPPB&5              
+          YP! 5&#GGPP5J7~~~~~~~~~7Y5PP5GGY                
+          ^PP  ?@JJ?7~~~~~~~~!!!!!~~!!PB:                 
+           7G5  YB?7!~~~~~~~~!   !~~~JG^                  
+            JGY  7G5?7!~~~~~~~___~~75Y:                   
+             ?GY5  ?P5Y?7777!!!7?J5Y~                     
+              !P5P5~:7JJ5GG7GP5YJ!:                       
+               k?5YPGPG55YJJYJJJYYP!                      
+                 ^Y#GPG5PYYJYJJ?YP#!                      
+                    !5BG5BPPPPPG?JYP                      
+                     JB5BGPPPPG?YP?                       
+                     !#5PPJJJJY?# G@                      
+                      YB55YJJJJGG G!                      
+                      .75PP55YJ~:  V                      
+                       7?     :7                          
+                      !PP.    ~#J                         
+                     .G!5^    ?5P!                        
+                     ?P~Y?    PJ?5                        
+                     PY~7G   !#7!P                        
+                     P577B   5BJ?5                        
+        ]],
       },
     },
   },
