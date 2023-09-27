ca6e85f5fe01eccf5acf82585b5f95b0f0c273a6
felec <101822531+fcrear@users.noreply.github.com>
2023-09-27 10:40:41 -0700

    :zap: rmv auto scroll

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -5,6 +5,9 @@ return {
     opts = {
       direction = "vertical",
       persist_mode = false,
+      auto_scroll = false,
+      insert_mappings = false,
+      terminal_mappings = false,
       size = function()
         return vim.o.columns * 0.25
       end,
