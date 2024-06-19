aa8f209105c6f0fb4e201d11cff7be77d451bee7
fele <43732967+felec@users.noreply.github.com>
2024-06-19 15:58:59 -0700

    :bug: Use new config formatters

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,27 +1,27 @@
 return {
   {
     "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        ["javascript"] = { "prettier" },
-        ["javascriptreact"] = { "prettier" },
-        ["typescript"] = { "prettier" },
-        ["typescriptreact"] = { "prettier" },
-        ["vue"] = { "prettier" },
-        ["css"] = { "prettier" },
-        ["scss"] = { "prettier" },
-        ["less"] = { "prettier" },
-        ["html"] = { "prettier" },
-        ["json"] = { "prettier" },
-        ["jsonc"] = { "prettier" },
-        ["markdown"] = { "prettier" },
-        ["markdown.mdx"] = { "prettier" },
-        ["graphql"] = { "prettier" },
-        ["handlebars"] = { "prettier" },
-        ["python"] = { "black" },
-        ["lua"] = { "stylua" },
-        ["yaml"] = { "yamlfmt" },
-      },
-    },
+    opts = function(_, opts)
+  if LazyVim.has_extra("formatting.prettier") then
+    opts.formatters_by_ft = opts.formatters_by_ft or {}
+    opts.formatters_by_ft.typescript = { "prettier" }
+    opts.formatters_by_ft.typescriptreact = { "prettier" }
+    opts.formatters_by_ft.javascript = { "prettier" }
+    opts.formatters_by_ft.javascriptreact = { "prettier" }
+    opts.formatters_by_ft.vue = { "prettier" }
+    opts.formatters_by_ft.css = { "prettier" }
+    opts.formatters_by_ft.scss = { "prettier" }
+    opts.formatters_by_ft.less = { "prettier" }
+    opts.formatters_by_ft.html = { "prettier" }
+    opts.formatters_by_ft.json = { "prettier" }
+    opts.formatters_by_ft.jsonc = { "prettier" }
+    opts.formatters_by_ft.markdown = { "prettier" }
+    opts.formatters_by_ft.graphql = { "prettier" }
+    opts.formatters_by_ft.handlebars = { "prettier" }
+    opts.formatters_by_ft.python = { "prettier" }
+    opts.formatters_by_ft.lua = { "prettier" }
+    opts.formatters_by_ft.yaml = { "prettier" }
+  end
+end
   },
 }
