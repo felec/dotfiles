65510d34e4cab11dbc0f47c9d978e6ee64faca81
Fele C <101822531+fcrear@users.noreply.github.com>
2026-03-23 07:50:42 -0700

    :recycle: Use claude, lazyvim version

--- nvim/lua/plugins/nvim-metals.lua
+++ nvim/lua/plugins/nvim-metals.lua
@@ -1,74 +1,73 @@
 return {
-  {
-    "scalameta/nvim-metals",
-    dependencies = { "nvim-lua/plenary.nvim", "mfussenegger/nvim-dap" },
-    config = function()
-      local metals_config = require("metals").bare_config()
-      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      local handlers = {
-        ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-        ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
-        -- ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-        --   underline = true,
-        --   update_in_insert = false,
-        --   float = { border = "rounded" },
-        --   virtual_text = { source = "if_many", spacing = 4, prefix = "●" },
-        --   severity_sort = true,
-        -- }),
-      }
-
-      if pcall(require, "cmp_nvim_lsp") then
-        capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-      end
-
-      metals_config.capabilities = capabilities
-      metals_config.handlers = handlers
-      metals_config.init_options.statusBarProvider = "on"
-      metals_config.settings = {
-        showInferredType = true,
-        showImplicitArguments = true,
-        superMethodLensesEnabled = true,
-        enableSemanticHighlighting = true,
-        showImplicitConversionsAndClasses = true,
-        scalafmtConfigPath = "/Users/fele/Dev/chatmeter/.scalafmt.conf",
-        javaHome = "/usr/local/Cellar/openjdk@17/17.0.15/libexec/openjdk.jdk/Contents/Home",
-        excludedPackages = {
-          "akka.actor.typed.javadsl",
-          "com.github.swagger.akka.javadsl",
-        },
-      }
-
-      require("dap").configurations.scala = {
-        {
-          type = "scala",
-          request = "launch",
-          name = "RunOrTest",
-          metals = {
-            runType = "runOrTestFile",
-          },
-        },
-        {
-          type = "scala",
-          request = "launch",
-          name = "Test Target",
-          metals = {
-            runType = "testTarget",
-          },
-        },
-      }
-
-      metals_config.on_attach = function(_, _)
-        require("metals").setup_dap()
-      end
-
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "scala", "sbt", "java" },
-        callback = function()
-          require("metals").initialize_or_attach(metals_config)
-        end,
-        group = nvim_metals_group,
-      })
-    end,
+  "scalameta/nvim-metals",
+  dependencies = {
+    {
+      "hrsh7th/nvim-cmp",
+      event = "InsertEnter",
+    },
+    {
+      "j-hui/fidget.nvim",
+      opts = {},
+    },
+    -- {
+    --   "mfussenegger/nvim-dap",
+    --   config = function(self, opts)
+    --     local dap = require("dap")
+    --
+    --     dap.configurations.scala = {
+    --       {
+    --         type = "scala",
+    --         request = "launch",
+    --         name = "RunOrTest",
+    --         metals = {
+    --           runType = "runOrTestFile",
+    --         },
+    --       },
+    --       {
+    --         type = "scala",
+    --         request = "launch",
+    --         name = "Test Target",
+    --         metals = {
+    --           runType = "testTarget",
+    --         },
+    --       },
+    --     }
+    --   end,
+    -- },
   },
+  ft = { "scala", "sbt", "java" },
+  opts = function()
+    local metals_config = require("metals").bare_config()
+
+    metals_config.settings = {
+      excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+      showInferredType = true,
+      showImplicitArguments = true,
+      superMethodLensesEnabled = true,
+      enableSemanticHighlighting = true,
+      showImplicitConversionsAndClasses = true,
+      scalafmtConfigPath = "/Users/fele/Dev/chatmeter/.scalafmt.conf",
+      javaHome = "/usr/local/Cellar/openjdk@17/17.0.15/libexec/openjdk.jdk/Contents/Home",
+    }
+
+    metals_config.init_options.statusBarProvider = "on"
+
+    metals_config.capabilities = vim.lsp.protocol.make_client_capabilities()
+
+    -- metals_config.on_attach = function(client, bufnr)
+    --   require("metals").setup_dap()
+    -- end
+
+    return metals_config
+  end,
+  config = function(self, metals_config)
+    local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = self.ft,
+      callback = function()
+        require("metals").initialize_or_attach(metals_config)
+      end,
+      group = nvim_metals_group,
+    })
+  end,
 }
